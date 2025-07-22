f299e7aa8e08533956f65d8d820e0e02b6e02dd1
Isaque Martins <isaque.martins.barros@gmail.com>
2025-07-22 20:03:07 -0300

    criando comandos e  interface cli

---
+++ src/presentation/app.ts
@@ -0,0 +1,26 @@
+class ChatCLI {}
+
+console.log(` ▄█          ▄████████    ▄████████    ▄████████
+███         ███    ███   ███    ███   ███    ███
+███▌        ███    █▀    ███    █▀    ███    █▀ 
+███▌        ███         ▄███▄▄▄      ▄███▄▄▄    
+███▌      ▀███████████ ▀▀███▀▀▀     ▀▀███▀▀▀    
+███                ███   ███    █▄    ███    █▄ 
+███          ▄█    ███   ███    ███   ███    ███
+█▀         ▄████████▀    ██████████   ██████████
+                                                
+▄██   ▄    ▄██████▄  ███    █▄                  
+███   ██▄ ███    ███ ███    ███                 
+███▄▄▄███ ███    ███ ███    ███                 
+▀▀▀▀▀▀███ ███    ███ ███    ███                 
+▄██   ███ ███    ███ ███    ███                 
+███   ███ ███    ███ ███    ███                 
+███   ███ ███    ███ ███    ███                 
+ ▀█████▀   ▀██████▀  ████████▀  
+ 
+Welcome to the ChatCLI!
+This is a command-line interface for interacting with a chat application.
+Dont use your real name here.
+This is a anonymous chat application.
+Type 'help' to see available commands.                         
+`);
