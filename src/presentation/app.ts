49cab8f6c0de327cb49dbe1d281d595828b23b76
Isaque Martins <isaque.martins.barros@gmail.com>
2025-07-24 00:12:26 -0300

    lendo input do usuario

--- src/presentation/app.ts
+++ src/presentation/app.ts
@@ -1,4 +1,8 @@
-class ChatCLI {}
+import { Help } from "../app/commands/help/Help";
+
+const readLine = require("node:readline");
+const { stdin: input, stdout: output } = require("node:process");
+const rl = readLine.createInterface({ input, output });
 
 console.log(` ▄█          ▄████████    ▄████████    ▄████████
 ███         ███    ███   ███    ███   ███    ███
@@ -24,3 +28,10 @@ Dont use your real name here.
 This is a anonymous chat application.
 Type 'help' to see available commands.                         
 `);
+
+rl.question("What you say? ", (answer: string) => {
+  if (answer === "help") {
+    const help = new Help();
+    help.helpResponse();
+  }
+});
